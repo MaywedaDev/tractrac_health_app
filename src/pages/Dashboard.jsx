12f3df2edeb827fb32dfa87f7bfde51aca88bfc1
Enyo <enyoonuche1@gmail.com>
2023-05-19 18:18:48 +0100

    Updated Code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout";
+import asset from '../assets/Patient-Dashboard-Overview-Light-Theme.jpg'
+import darkAsset from '../assets/Section67.jpg'
+import userThemeContext from "../stores/userThemeContext";
+import { useContext } from "react";
 const Dashboard = () => {
+    const user = useContext(userThemeContext)
     return (  
         <Layout>
-            <div className="px-6">
-
+            <div className="px-6 w-full h-full">
+                <img className="w-full h-full" src={user.theme ? darkAsset : asset} alt="" />
             </div>
             
         </Layout>
